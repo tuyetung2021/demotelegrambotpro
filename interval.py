8366dcc426a387f409e6b5da7036eb2a2eeb01ec
tuyetung2021 <113813405+tuyetung2021@users.noreply.github.com>
2022-09-18 15:49:18 +0700

    Delete interval.py

--- interval.py
+++
@@ -1,9 +0,0 @@
-from threading import Timer
-import time
-
-
-def hiddenInterval(update, context):
-    while True:       
-        print("hiddenInterval")
-        update.message.reply_text('The Price : '+str(context.args[0]))
-        time.sleep(3)
