cf6d4dc311513c3b4b0ca2ed24cbce47b8f27932
tuyetung2021 <113813405+tuyetung2021@users.noreply.github.com>
2022-09-18 16:23:22 +0700

    Update app.py

--- app.py
+++ app.py
@@ -8,70 +8,10 @@ from telegram import InlineKeyboardButton, InlineKeyboardMarkup
 TOKEN = os.environ.get("TOKEN")
 
 #commandhandler for start command
-def start2(update, context):
+def start(update, context):
     msg = "Hi ! Welcome to Demo Telegram Bot Pro."
     context.bot.send_message(update.message.chat.id, msg)
     
-def start(update, context):
-  update.message.reply_text(main_menu_message(),
-                            reply_markup=main_menu_keyboard())
-
-def main_menu(update,context):
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-                        text=main_menu_message(),
-                        reply_markup=main_menu_keyboard())
-
-def first_menu(update,context):
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-                        text=first_menu_message(),
-                        reply_markup=first_menu_keyboard())
-
-def second_menu(update,context):
-  query = update.callback_query
-  query.answer()
-  query.edit_message_text(
-                        text=second_menu_message(),
-                        reply_markup=second_menu_keyboard())
-
-# and so on for every callback_data option
-def first_submenu(bot, update):
-  pass
-
-def second_submenu(bot, update):
-  pass
-
-############################ Keyboards #########################################
-def main_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Option 1', callback_data='m1')],
-              [InlineKeyboardButton('Option 2', callback_data='m2')],
-              [InlineKeyboardButton('Option 3', callback_data='m3')]]
-  return InlineKeyboardMarkup(keyboard)
-
-def first_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Submenu 1-1', callback_data='m1_1')],
-              [InlineKeyboardButton('Submenu 1-2', callback_data='m1_2')],
-              [InlineKeyboardButton('Main menu', callback_data='main')]]
-  return InlineKeyboardMarkup(keyboard)
-
-def second_menu_keyboard():
-  keyboard = [[InlineKeyboardButton('Submenu 2-1', callback_data='m2_1')],
-              [InlineKeyboardButton('Submenu 2-2', callback_data='m2_2')],
-              [InlineKeyboardButton('Main menu', callback_data='main')]]
-  return InlineKeyboardMarkup(keyboard)
-
-############################# Messages #########################################
-def main_menu_message():
-  return 'Choose the option in main menu:'
-
-def first_menu_message():
-  return 'Choose the submenu in first menu:'
-
-def second_menu_message():
-  return 'Choose the submenu in second menu:'
 
 #Error handler
 def error(update, context):
@@ -128,15 +68,6 @@ def main():
     dp.add_handler(CommandHandler("minus", minus))
     dp.add_handler(CommandHandler("mul", mul))
     dp.add_handler(CommandHandler("div", div))
-    
-    dp.add_handler(CommandHandler('start2', start))
-    dp.add_handler(CallbackQueryHandler(main_menu, pattern='main'))
-    dp.add_handler(CallbackQueryHandler(first_menu, pattern='m1'))
-    dp.add_handler(CallbackQueryHandler(second_menu, pattern='m2'))
-    dp.add_handler(CallbackQueryHandler(first_submenu,
-                                                    pattern='m1_1'))
-    dp.add_handler(CallbackQueryHandler(second_submenu,
-                                                    pattern='m2_1'))
 
     #dp.add_handler(MessageHandler(Filters.text, mimic))
 
